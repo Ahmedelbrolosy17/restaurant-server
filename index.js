9186d16fbea1ee6b0ef405db9b37af662ac0742c
ahmed <am3450931@gmail.com>
2024-10-14 19:31:54 +0300

    fisrt commit

---
+++ index.js
@@ -0,0 +1,58 @@
+require('dotenv').config();
+const express = require('express');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+const cors = require('cors');
+
+// Enable CORS
+app.use(cors({
+  origin: 'https://restaurant-front-r20yy6hh3.vercel.app'
+}));
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+
+app.get('/', (req, res)=>{
+  res.end("welcome to our server")
+})
+app.get('/about', (req, res)=>{
+  res.end("this is a server for a payment process")
+})
+app.post('/checkout', async (req, res) => {
+  const { basket } = req.body;
+
+  // Prepare line items for Stripe from the basket data
+  const line_items = basket.map(item => ({
+    price_data: {
+      currency: 'usd',
+      product_data: {
+        name: item.name,
+      },
+      unit_amount: Math.round(item.price * (1 - item.discount) * 100), // Convert to cents
+    },
+    quantity: item.quantity,
+  }));
+
+  // Create Stripe checkout session
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items,
+      mode: 'payment',
+      success_url: `${process.env.BASE_URL}/complete`,
+      cancel_url: `${process.env.BASE_URL}/order`,
+    });
+
+    // Send the session URL back to the frontend
+    res.json({ url: session.url });
+  } catch (error) {
+    console.error('Error creating Stripe session:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.listen(5000, () => {
+  console.log('Server started on port 5000');
+});
